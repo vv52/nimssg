2a787f29df0531b8851e0b5563f676f0f5a4fd95
Vanja <vanjavenezia@gmail.com>
2025-08-02 05:38:41 -0700

    add automatic loading of posts, add folder structure, make generatePage agnostic

---
+++ posts/test.md
@@ -0,0 +1,7 @@
+TEST POST
+1 AUG 2025
+# Test post
+* some stuff
+* some more stuff
+## Ok
+lorem ipsumma deez nuts
--- ssg.nim
+++ ssg.nim
@@ -1,37 +1,68 @@
-import std/streams
+import std/streams, std/tables, std/strbasics, std/os, std/sequtils
 
-var final = newFileStream("index.html")
-var tmpl = newStringStream(readFile("index.tmpl"))
-var outp = newStringStream()
-var inst = newStringStream()
-var content : bool = false
-var buffer: string
+proc getPosts(): seq[string]
+proc generatePage(templateFile: string, contentFile: string, outfile: string, isPost: bool): string
+proc importContent(contentFile: string, isPost: bool): Table[string, string]
+proc main(): void
 
-while not tmpl.atEnd():
-  tmpl.peekStr(2, buffer)
-  if not content:
-    if buffer == "<%":
-      content = true
-      try: discard tmpl.readStr(2)
-      except: break
+proc getPosts(): seq[string] =
+  result = toSeq(walkFiles("posts/*.md"))
+
+proc generatePage(templateFile: string, contentFile: string, outfile: string, isPost: bool): string =
+  var sTemplateFile = newStringStream(readFile(templateFile))
+  var sOutstr = newStringStream()
+  var sContent = newStringStream()
+  var isContent : bool = false
+  var buffer: string
+  var content : Table[string, string] = importContent(contentFile, isPost)
+
+  while not sTemplateFile.atEnd():
+    sTemplateFile.peekStr(2, buffer)
+    if not isContent:
+      if buffer == "<%":
+        isContent = true
+        try: discard sTemplateFile.readStr(2)
+        except: discard
+      else:
+        try: sOutstr.write($sTemplateFile.readStr(1))
+        except: discard
     else:
-      try: outp.write($tmpl.readStr(1))
-      except: break
+      if buffer == "%>":
+        isContent = false
+        try:
+          discard sTemplateFile.readStr(2)
+          sContent.setPosition(0)
+          var contentTag : string = $sContent.readAll()
+          contentTag.strip()
+          sOutstr.write($content[contentTag])
+          sContent = newStringStream()
+        except: discard
+      else:
+        try: sContent.write($sTemplateFile.readStr(1))
+        except: discard
+  sOutstr.setPosition(0)
+  result = sOutstr.readAll()
+  sOutstr.setPosition(0)
+  syncio.writeFile(outfile, sOutstr.readAll())
+  sTemplateFile.close()
+  sOutstr.close()
+
+proc importContent(contentFile: string, isPost: bool): Table[string, string] =
+  var sContent = newStringStream(readFile(contentFile))
+  if isPost:
+    var title : string = sContent.readLine()
+    var date : string = sContent.readLine()
+    var body : string = sContent.readAll()
+    result = {"TITLE": title, "DATE": date, "BODY": body}.toTable
   else:
-    if buffer == "%>":
-      content = false
-      try:
-        discard tmpl.readStr(2)
-        inst.write("\n")
-      except: break
-    else:
-      try: inst.write($tmpl.readStr(1))
-      except: break
-outp.setPosition(0)
-inst.setPosition(0)
-echo outp.readAll()
-echo inst.readAll()
-# final.write(outp.readAll())
-tmpl.close()
-outp.close()
-final.close()
+    var title : string = sContent.readLine()
+    var body : string = sContent.readAll()
+    result = {"TITLE": title, "BODY": body}.toTable
+
+proc main =
+  for post in getPosts():
+    echo generatePage("templates/index.tmpl", post, "public/" & post, true)
+  echo generatePage("templates/index.tmpl", "pages/index.md", "public/index.html", false)
+
+when isMainModule:
+  main()
