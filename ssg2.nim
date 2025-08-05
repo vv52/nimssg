4aa635e716ebfabf27cb9d61a73846a0149be2be
Vanja <vanjavenezia@gmail.com>
2025-08-05 19:21:09 +0000

    add Content object to replace tuple of table[str,str] and str, add getPagesObj() and omportContentObj() to test this data model ahead of full transition

---
+++ ssg2.nim
@@ -0,0 +1,194 @@
+import std/os, std/times, std/streams, std/tables
+import std/strutils, std/strformat
+import std/sequtils
+import std/algorithm
+import markdown, dekao
+
+# TODO: Post and Page objects that hold tags and body content
+#       Rewrite everything to use these
+
+type
+  Content = object
+    path*: string
+    title*: string
+    description*: string
+    date*: int
+    body*: string
+    # tags*: Table[string, string]
+    # content*: string
+    ## maybe this instead?
+
+proc setup() : void
+proc build() : void
+proc initDistDir() : void
+proc getPages() : seq[string]
+proc getPagesObj() : seq[Content]
+proc getPosts() : seq[string]
+proc generateHead(content_title: string) : string
+proc generateHeader(content_title: string, content_description: string = "") : string
+proc generateFooter(email_address: string) : string
+proc generatePage(page_path: string) : string
+proc generatePost(post_path: string) : string
+proc generateBlog() : OrderedTable[string, int]
+proc importContent(content_file : string) : (Table[string, string], string)
+proc importContentObj(content_file : string) : Content
+proc main() : void
+
+proc setup() : void =
+  echo "Checking prerequisite directories:"
+  if existsOrCreateDir("pages/"): echo "  Pages \u2713"
+  else: echo "  Pages directory not found\n    Creating pages directory...\n    Pages \u2713"
+  if existsOrCreateDir("posts/"): echo "  Posts \u2713"
+  else: echo "  Posts directory not found\n    Creating posts directory...\n    Posts \u2713"
+
+proc build() : void =
+  initDistDir()
+  for page in getPages():
+    # maybe render index.html to root but all else to pages?
+    let path : string = fmt".dist/{page.split('.')[0].split('/')[1]}.html"
+    writeFile(path, generatePage(page))
+  for post in getPosts():
+    let path : string = fmt".dist/{post.split('.')[0]}.html"
+    writeFile(path, generatePost(post))
+  if fileExists("custom.css"):
+    copyFileToDir("custom.css", ".dist/")
+
+proc initDistDir() : void =
+  removeDir(".dist/")
+  createDir(".dist/")
+#  createDir(".dist/pages/")
+  createDir(".dist/posts/")
+
+proc getPages() : seq[string] =
+  result = toSeq(walkFiles("pages/*.md"))
+
+proc getPagesObj() : seq[Content] =
+  let page_paths = toSeq(walkFiles("pages/*.md"))
+  var pages : seq[Content]
+  for page_path in page_paths:
+    pages.add(importContentObj(page_path))
+  result = pages
+
+proc getPosts() : seq[string] =
+  result = toSeq(walkFiles("posts/*.md"))
+
+proc generateHead(content_title: string) : string =
+  result = render:
+    head:
+      meta: charset "utf-8"
+      meta: name "viewport"; content "width=device-width, initial-scale=1.0"
+      meta: httpEquiv "X-UA-Compatible"; content "ie=edge"
+      link: rel "stylesheet"; href "https://cdn.simplecss.org/simple.min.css"
+      link: rel "stylesheet"; href "/custom.css"
+      link: rel "icon"; href "./favicon.ico"; ttype "image/x-icon"
+      title: say content_title
+
+proc generateHeader(content_title: string, content_description: string = "") : string =
+  result = render:
+    header:
+      nav:
+        a: href "/"; class "current"; say "Home"
+        a: href "/blog"; say "Blog"
+        a: href "https://github.com/vv52"; say "GitHub"
+        a: href "https://vexingvoyage.itch.io"; say "itch.io"
+      h1: say content_title
+      p: say content_description
+
+proc generateFooter(email_address: string) : string =
+  result = render:
+    footer:
+      p:
+        a: href fmt"mailto:{email_address}"; say email_address
+
+proc generatePage(page_path: string) : string =
+  let imported_content = importContent(page_path)
+  var tags : Table[string, string] = imported_content[0]
+  let content : string = imported_content[1]
+  if tags.hasKeyOrPut($"description", $""):
+    discard
+  result = fmt"""<!DOCTYPE html>
+                 <html lang="en">
+                 {generateHead(tags["title"])}
+                 <body>{generateHeader(tags["title"], tags["description"])}
+                 <main>{content}</main>
+                 {generateFooter("vanjavenezia@gmail.com")}
+                 </body></html>"""
+                 
+proc generatePost(post_path: string) : string =
+  let imported_content = importContent(post_path)
+  let tags : Table[string, string] = imported_content[0]
+  let content : string = imported_content[1]
+  let raw_post_date = parse(tags["date"], "yyyyMMdd")
+  let post_date = raw_post_date.format("d MMMM yyyy")
+  result = fmt"""<!DOCTYPE html>
+                 <html lang="en">
+                 {generateHead(tags["title"])}
+                 <body>{generateHeader(tags["title"], post_date)}
+                 <main>{content}</main>
+                 {generateFooter("vanjavenezia@gmail.com")}
+                 </body></html>"""
+
+proc generateBlog() : OrderedTable[string, int] =
+  var posts = initOrderedTable[string, int]()
+  for post in getPosts():
+    let path : string = fmt".dist/{post.split('.')[0]}.html"
+    writeFile(path, generatePost(post))
+    let imported_content = importContent(post)
+    let tags : Table[string, string] = imported_content[0]
+    posts[post] = tags["date"].parseInt()
+  posts.sort(system.cmp, order = SortOrder.Descending)
+  var body_content = """<ul>"""
+  for dated_post in posts.keys:
+    body_content = body_content & fmt"""<li><a href="/{dated_post.split('.')[0]}.html">{dated_post}</a>"""
+  body_content = body_content & """</ul>"""
+  echo posts
+  echo body_content
+  result = posts
+
+proc importContent(content_file : string) : (Table[string, string], string) =
+  let content = newStringStream(readFile(content_file))
+  var is_frontmatter : bool = false
+  var tags : Table[string, string] = initTable[string, string]()
+  var body : string = """"""
+  var buffer : string = ""
+  buffer = content.readLine()
+  if buffer == "---": is_frontmatter = true
+  while is_frontmatter:
+    buffer = content.readLine()
+    if buffer == "---": is_frontmatter = false
+    else: tags[buffer.split(':')[0].strip()] = buffer.split(':')[1].strip()
+  body = markdown(content.readAll())
+  result = (tags, body)
+
+proc importContentObj(content_file : string) : Content =
+  let imported_content = newStringStream(readFile(content_file))
+  var is_frontmatter : bool = false
+  var buffer : string = ""
+  var property : string = ""
+  var exported_content = Content(path: content_file)
+  buffer = imported_content.readLine()
+  if buffer == "---": is_frontmatter = true
+  while is_frontmatter:
+    buffer = imported_content.readLine()
+    if buffer == "---": is_frontmatter = false
+    else:
+      property = buffer.split(':')[0].strip()
+      case property
+      of "title": 
+        exported_content.title = buffer.split(':')[1].strip()
+      of "description":
+        exported_content.description = buffer.split(':')[1].strip()
+      of "date":
+        exported_content.date = buffer.split(':')[1].strip().parseInt()
+      else:
+        discard
+  exported_content.body = markdown(imported_content.readAll())
+  result = exported_content
+
+proc main =
+  setup()
+  build()
+  discard generateBlog()
+
+when isMainModule:
+  main()
